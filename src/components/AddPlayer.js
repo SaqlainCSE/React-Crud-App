4b4b784b7db02cbaf36d896e7ecd9281fcc5defc
SaqlainCSE <56887504+SaqlainCSE@users.noreply.github.com>
2020-11-19 18:12:12 +0600

    Initialize Axios

---
+++ src/components/AddPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddPlayer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddPlayer
---
+++ src/components/PlayerList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayerList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PlayerList
