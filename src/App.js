5a6c8aa619c376fcde4a5798b791aedc1fa722b4
SaqlainCSE <56887504+SaqlainCSE@users.noreply.github.com>
2020-11-19 15:48:43 +0600

    NavBar Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import PlayerList from './components/PlayerList';
+import AddPlayer from './components/AddPlayer';
+import Player from './components/Player';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+
+        <a href="/players" className="navbar-brand">
+          React Crud App
         </a>
-      </header>
+
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/players"} className="nav-link">
+              Players
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+
+      </nav>
+
+      <div className="container mt-3">
+
+        <Switch>
+            <Route exact path={["/", "/players"]}  component={PlayerList} />
+            <Route exact path="/add" component={AddPlayer} />
+            <Route path="/players/:id" component={Player} />
+        </Switch>
+
+      </div>
+
     </div>
   );
 }
