4192a788ce03063a91a207f4504f9c02b725c776
SaqlainCSE <56887504+SaqlainCSE@users.noreply.github.com>
2020-12-02 20:18:33 +0600

    Update Data Service

--- src/services/PlayerService.js
+++ src/services/PlayerService.js
@@ -2,37 +2,37 @@ import http from '../http-common';
 
  const getAll = () => {
 
-    return http.get("/players");
+    return http.get("/tutorials");
 };
 
  const get = (id) => {
 
-    return http.get(`/players/${id}`);
+    return http.get(`/tutorials/${id}`);
 };
 
  const create = (data) => {
 
-    return http.post("/players", data);
+    return http.post("/tutorials", data);
 };
 
  const update = (id, data) => {
 
-    return http.put(`/players/${id}`, data);
+    return http.put(`/tutorials/${id}`, data);
 };
 
  const remove = (id) => {
 
-    return http.delete(`/players/${id}`);
+    return http.delete(`/tutorials/${id}`);
 };
 
  const removeAll = () => {
 
-    return http.delete("/players");
+    return http.delete("/tutorials");
 };
 
  const findByTitle = (title) => {
 
-    return http.get(`/players?title=${title}`);
+    return http.get(`/tutorials?title=${title}`);
 };
 
 
